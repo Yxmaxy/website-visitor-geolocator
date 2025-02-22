3a266e15644aab33f9c5572ce5b47c429259ec3e
Yxmaxy <vrecer.marko@gmail.com>
2025-02-22 17:29:33 +0100

    Moved views

---
+++ backend/visitor_geolocator/api/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpRequest, JsonResponse
+
+from visitor_geolocator.models import Domain, GeolocationAPI
+
+
+def ping(request: HttpRequest) -> JsonResponse:
+    return JsonResponse({"message": "pong"})
+
+
